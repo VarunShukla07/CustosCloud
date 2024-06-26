ba0b5f3a60cd3effc51fbb6004a6af3ce2bbb84f
VarunShukla07 <94779584+VarunShukla07@users.noreply.github.com>
2024-06-26 23:36:06 +0530

    Account setting update page.js

--- app/(dashboard)/(routes)/upgrade/page.js
+++ app/(dashboard)/(routes)/upgrade/page.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import { UserProfile } from '@clerk/nextjs';
+import React from 'react';
 
 function Upgrade() {
   return (
-    <div>
-      Upgrade
+    <div className='flex items-center justify-center h-full'>
+      <UserProfile routing="hash" />
     </div>
-  )
+  );
 }
 
-export default Upgrade
-Upgrade
+export default Upgrade;
